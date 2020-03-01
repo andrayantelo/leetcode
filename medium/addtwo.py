a4478d4671a62e5196b3745bf069cdc9b7a93ceb
andrayantelo <andrayantelo@gmail.com>
2020-03-01 12:22:36 -0800

    working on a medium leetcode problem

---
+++ medium/addtwo.py
@@ -0,0 +1,34 @@
+"""
+You are given two non-empty linked lists representing two non-negative integers.
+The digits are stored in reverse order and each of their nodes contain a single digit.
+Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+Example:
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+"""
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        print(l1.val)
+
+
+if __name__ == "__main__":
+    l1 = ListNode(2)
+    l1.next = ListNode(4)
+    l1.next.next = ListNode(3)
+
+    l2 = ListNode(5)
+    l2.next = ListNode(6)
+    l2.next = ListNode(4)
+
+    solution = Solution()
+    solution.addTwoNumbers(l1, l2)
