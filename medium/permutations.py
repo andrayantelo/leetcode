778b0997e51d80c52e0de75bf6121ac33badc440
andrayantelo <andrayantelo@gmail.com>
2020-09-02 10:51:43 -0400

    starting medium level problem

---
+++ medium/permutations.py
@@ -0,0 +1,26 @@
+"""
+Leetcode 46
+Given a collection of distinct integers, return all possible permutations.
+
+Example:
+Input: [1,2,3]
+Output:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+"""
+
+class Solution:
+    def permute(self, nums):
+        pass
+
+if __name__ == "__main__":
+    solution = Solution()
+    nums = [1, 2, 3]
+    solution.permute(nums)
+        
