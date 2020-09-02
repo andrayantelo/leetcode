dfe0ffed72b4b41a5fbf651b9c2d46856506da48
andrayantelo <andrayantelo@gmail.com>
2020-09-02 09:37:41 -0400

    rudimental solution

--- easy/commonprefix.py
+++ easy/commonprefix.py
@@ -15,8 +15,23 @@ Explanation: There is no common prefix among the input strings.
 """
 
 def common_prefix(words):
-    pass
+    prev_prefix = ""
+    common_prefix = ""
+    for i, letter in enumerate(words[0]):
+        common_prefix += words[0][i]  # n^2
+        print("current common prefix ", common_prefix)
+        for j in range(1, len(words)):
+            # startswith method
+            print("checking word ", words[j])
+            print(words[j][:i + 1])
+            if words[j][:i + 1] != common_prefix:
+                print(common_prefix, " not common")
+                return prev_prefix
+        prev_prefix = common_prefix
+    return common_prefix
+                
 
-if __name___ == "__main__":
+if __name__ == "__main__":
     words = ["flower", "flow", "flight"]
-    common_prefix(words)
+
+    print(common_prefix(words))
