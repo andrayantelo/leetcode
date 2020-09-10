845929a63405d26e18a63fb733278b5942267227
andrayantelo <andrayantelo@gmail.com>
2020-09-10 12:30:48 -0400

    worked on different versions of binary addition problem

--- easy/binaryaddition.py
+++ easy/binaryaddition.py
@@ -15,21 +15,53 @@ class Solution():
         for i in range(len(a) - 1, -1, -1):
             my_var = a[i] + b[i] + carry_over
             # if result > 1, you have a carry over
-            if my_var > 1:
-                carry_over = 1
-                c.append(0)
-            else:
-                c.append(my_var)
+            carry_over = 1 if my_var > 1 else 0
+            c.append(my_var % 2)
+        if carry_over:
+            c.append(carry_over)
+        # c was built backwards so reverse it
+        c.reverse()
+        return c
+
+    def bin_add_diff(self, a, b):
+        # the same as before except iterate over the smaller one
+        c = []
+        carry_over = 0
+        # make a be the smaller one
+        a, b = sorted([a, b], key=len)
+        # pad a so that a and b are the same length
+        padding = len(b) - len(a)
+        a = [0]*padding + a
+        for i in range(len(b) - 1, -1, -1):
+            my_var = a[i] + b[i] + carry_over
+            # if result > 1, you have a carry over
+            carry_over = 1 if my_var > 1 else 0
+            c.append(my_var % 2)
         if carry_over:
             c.append(carry_over)
         # reverse c
-        return c[::-1]
-
-    def bin_add_diff(self, a, b):
-        pass
+        c.reverse()
+        return c 
 
     def bin_add_str(self, a, b):
-        pass
+        # make a be the smaller one so that we don't have to
+        # do a bunch of if-else statements to figure it out
+        a, b = sorted([a, b], key=len)
+        # pad a with 0's so that a and b are the same length
+        a = a.zfill(len(b))
+        c = ""
+        carry_over = 0
+        for i in range(len(b) - 1, -1, -1):
+            my_var = int(a[i]) + int(b[i]) + carry_over
+            # if result > 1, you have a carry over
+            carry_over = 1 if my_var > 1 else 0
+            c = c + str(my_var % 2)
+        if carry_over:
+            c = c + str(carry_over)
+        # reverse c
+        c = c[::-1]
+        return c
+
 
 class TestBinaryAdd(unittest.TestCase):
 
@@ -41,9 +73,30 @@ class TestBinaryAdd(unittest.TestCase):
         b = [1, 0, 0]
 
         self.assertEqual(self.solution.bin_add_same(a, b), [1, 0, 1, 1])
+        a = [1, 1, 1]
+        b = [1, 0]
+        self.assertEqual(self.solution.bin_add_diff(a, b), [1, 0, 0, 1])
+        b = [1, 1, 1]
+        self.assertEqual(self.solution.bin_add_same(a, b), [1, 1, 1, 0])
+        b = [1, 1]
+        self.assertEqual(self.solution.bin_add_diff(a, b), [1, 0, 1, 0])
 
     def test_empty(self):
         self.assertEqual(self.solution.bin_add_same([],[]), [])
+        self.assertEqual(self.solution.bin_add_diff([], []), [])
+
+    def test_bin_str(self):
+        a = "111"
+        b = "100"
+        self.assertEqual(self.solution.bin_add_str(a, b), "1011")
+        a = "111"
+        b = "10"
+        self.assertEqual(self.solution.bin_add_str(a, b), "1001")
+        b = "111"
+        self.assertEqual(self.solution.bin_add_str(a, b), "1110")
+        b = "11"
+        self.assertEqual(self.solution.bin_add_str(a, b), "1010")
+        self.assertEqual(self.solution.bin_add_str("", ""), "")
 
 if __name__ == "__main__":
     unittest.main()
@@ -52,3 +105,11 @@ if __name__ == "__main__":
 
     #solution = Solution()
     #print(solution.bin_add(a, b))
+    #a = [1, 1, 1]
+    #b = [1, 0]
+    #print(solution.bin_add_diff(a, b))
+
+    #a = "111"
+    #b = "100"
+    #print(solution.bin_add_str(a, b))
+    
